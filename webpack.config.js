e39c6344d05a53988ab2da0cfaedc6aff5ab4db9
jeff.cooper <jeff.cooper@servicenow.com>
2017-11-19 08:22:12 -0800

    Compress, but don't mangle

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: {
     photo_handlers: ['./src/app/handlers/photo_handlers.js']
   },
   plugins: [
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['dist']),
+    new UglifyJSPlugin({
+      beautify: true,
+      compress: true,
+      mangle: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
   ],
   output: {
     path: path.resolve(__dirname, 'dist/handlers'),
